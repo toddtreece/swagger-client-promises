ea3018006c5e72b7ab918c751716e75bf0af6486
Todd Treece <toddtreece@gmail.com>
2015-09-17 13:16:23 -0400

    adds mocha test

---
+++ test/api.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const client = require('../index');
+let swagger = false;
+
+
+describe('Swagger Promises', function() {
+
+  this.timeout(10000);
+
+  describe('Init', function() {
+
+    it('should create a client', function(done) {
+
+      swagger = new client({
+        url: 'http://petstore.swagger.io/v2/swagger.json',
+        success: done,
+        failure: done
+      });
+
+    });
+
+  });
+
+  describe('Operation', function() {
+
+    it('should return a promise', function(done) {
+
+      swagger.pet.getPetById({petId:7}, {responseContentType: 'application/json'})
+      .then(pet => {
+        if(pet.status === 200) done();
+      })
+      .catch(done);
+
+    });
+
+  });
+
+});
